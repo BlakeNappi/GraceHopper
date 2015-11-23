402c24ff4b96555a190ca88a5be68b8dd7c19ce0
incrinage <jn13baller@hotmail.com>
2015-11-23 14:15:16 -0500

    Create Grace.java

---
+++ Grace.java
@@ -0,0 +1,13 @@
+
+public class Grace {
+	
+	private double x,y;
+	private int  width, height;
+	private double gravity,speedV,speedH;
+	private double weight;
+	
+	public void moveLeft(){}
+	public void moveRight(){}
+	public void setHorizontalSpeed(double sH ){}
+	public void drawGrace(){}
+}
