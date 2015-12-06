665c430a428c1f3b511e2e818d80bc172cae8540
BlakeNappi <blake_nappi@my.uri.edu>
2015-12-06 15:20:19 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,2 +1,74 @@
-public class Game{
-}
+/**
+ * Game class is the main class of the game. It holds
+ * all of the objects which make the game run
+ * @author BlueBird
+ *
+ */
+
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class Game extends Frame implements MouseListener, KeyListener {
+
+	private Grace Grace;
+	private Abutton [] Buttons;
+	
+	public Game(){
+		
+		Grace = new Grace();
+		
+	}//Game
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}//end Game
