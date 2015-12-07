550f104e4777b84b57dc98b20aa1333943f76660
malvinherrera26 <malv.herrera@yahoo.com>
2015-12-07 15:05:27 -0500

    Update GrassHopper.java

--- GrassHopper.java
+++ GrassHopper.java
@@ -1,14 +1,41 @@
+import java.util.awt*;
 
 public class GrassHopper 
 {
     private GrassHopper grasshopper = new GrassHopper(); // The class instance of a grasshopper
     
     public int movement; // The movement for the grasshoppers
-    public int x, y; // X and Y coordinates for grasshopper positions
-    public int points;
     
+    public int x, y; // X and Y coordinates for grasshopper positions
+    
+    public int points; //
+    
+    public int hops, hopLim, hopD; // The hops each grasshopper will have 
+                                            // and their limits
+    
+    
+    /**
+     * This method will set up the movements for
+     * each grasshopper
+     * and set up a limit for them to stop moving
+     **/
     Public Grasshopper()
     {
-      
+      this.hops = 2;
+      this.hopLim = 3;
+    }
+    
+    public int randomHopMovement() 
+    {
+        
+    }
+    
+    /**
+     * The points for the grasshoppers will be
+     * set up within this method
+     **/ 
+    public int hopperPoints() 
+    {
+        
     }
 } // End GrassHopper 
