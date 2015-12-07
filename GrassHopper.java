d8d52e7586e51a21648babe279cf7c35f897a7d7
malvinherrera26 <malv.herrera@yahoo.com>
2015-12-07 14:39:28 -0500

    Update GrassHopper.java

--- GrassHopper.java
+++ GrassHopper.java
@@ -1,4 +1,14 @@
 
-public class GrassHopper {
-
-}
+public class GrassHopper 
+{
+    private GrassHopper grasshopper = new GrassHopper(); // The class instance of a grasshopper
+    
+    public int movement; // The movement for the grasshoppers
+    public int x, y; // X and Y coordinates for grasshopper positions
+    public int points;
+    
+    Public Grasshopper()
+    {
+      
+    }
+} // End GrassHopper 
