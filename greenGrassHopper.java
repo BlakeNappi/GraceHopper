edcf9756185f5ca770e5ce4a32a7e7c8dc374d29
malvinherrera26 <malv.herrera@yahoo.com>
2015-12-07 15:28:23 -0500

    Update greenGrassHopper.java

--- greenGrassHopper.java
+++ greenGrassHopper.java
@@ -4,6 +4,6 @@ public class greenGrassHopper() extends GrassHopper
   public greenGrassHopper() 
   {
     score = 25; // Score points for the green grasshopper
-  }
+  } // End constructor
   
-} // End greenGrassHopper
+} // End greenGrassHopper class
