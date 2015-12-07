c470a9bf48cb915bb00e9ceca933a2bba7b3d60b
malvinherrera26 <malv.herrera@yahoo.com>
2015-12-07 15:27:45 -0500

    Create brownGrassHopper.java

---
+++ brownGrassHopper.java
@@ -0,0 +1,10 @@
+public class brownGrassHopper() extends GrassHopper
+{
+  
+  public brownGrassHopper() 
+  {
+    score = 50; // Score points for the brown grasshopper
+    
+  } // End brownGrassHopper constructor
+  
+} // End brownGrassHopper class
