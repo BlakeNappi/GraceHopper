88d2eb4ffd17b46eba5cb78506563ea6be866efc
malvinherrera26 <malv.herrera@yahoo.com>
2015-12-07 15:24:58 -0500

    Update redGrassHopper.java

--- redGrassHopper.java
+++ redGrassHopper.java
@@ -1,5 +1,10 @@
 
-public class redGrassHopper()
+public class redGrassHopper() extends GrassHopper
 {
   
-}
+  public redGrassHopper() 
+  {
+    score = 100; // Score points for the red grasshopper
+  }
+  
+} // End redGrassHopper
