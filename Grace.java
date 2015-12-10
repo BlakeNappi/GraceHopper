1e9f9b3df00208693884efbec8ef16ad70cec5ea
Malvin Herrera <malv.herrera@yahoo.com>
2015-12-10 15:51:50 -0500

    Update Grace.java

--- Grace.java
+++ Grace.java
@@ -2,12 +2,23 @@
 public class Grace {
 	
 	private double x,y;
+	
 	private int  width, height;
+	
 	private double gravity,speedV,speedH;
+	
 	private double weight;
 	
-	public void moveLeft(){}
-	public void moveRight(){}
-	public void setHorizontalSpeed(double sH ){}
-	public void drawGrace(){}
+	public void moveLeft(){
+		
+	}
+	public void moveRight(){
+		
+	}
+	public void setHorizontalSpeed(double sH ){
+		
+	}
+	public void drawGrace(){
+		
+	}
 }
