7c92dd88cb5268238129dd4556952805b2c5c405
Malvin Herrera <malv.herrera@yahoo.com>
2015-12-12 15:33:47 -0500

    Update Grace.java

--- Grace.java
+++ Grace.java
@@ -6,37 +6,58 @@ public class Grace {
 	
 	private int  width, height; // Grace's width and height
 	
-	private double gravity,speedV,speedH;
+	private double gravity,speedV,speedH; //
 	
 	private double weight; // Grace's weight that will make her come back down after jumping
 	
-	private boolean moveLeft, moveRight, jump = false; // Booleans for Grace's movements
+	private boolean moveLeft, moveRight, jumping = false; // Booleans for Grace's movements
 	
 	public void moveLeft(){
 		moveLeft = true;
-	}
+	}// moveRLeft
 	
 	public void moveRight(){
 		moveRight = false;
-	}
+	}// moveRight
 	
 	public void jump() {
-		jump = false;
-	}
+		jumping = true;
+		jumpMotion();
+	}// jump
+	
+	private void jumpMotion(){
+		int counter = 0;
+		while(jumping){
+			if(counter < 5){
+				y+=speedV;
+				counter++;
+			}
+			else if(counter == 5){
+				y-=speedV;
+			}else{
+				jumping = false;
+			}
+		}	
+	}// jumpMotion
+	
+	public boolean getJump(){
+		return jumping;
+	}// getJump
+	
 	public void moving(){
 		while(true) {
-			if(moveLeft && !moveRight) 
+			if(moveLeft && !moveRight) // Grace moves left
 				x-=speedH;
-			if(!moveLeft && moveRight)
+			if(!moveLeft && moveRight) // Grace moves right
 				x+=speedH;
-		}
-	}
+		}// while loop
+	}// moving
 	
 	public void setHorizontalSpeed(double sH ){
 		
-	}
+	}// setHorizontalSpeed
 	
 	public void drawGrace(){
 		
-	}
-}
+	}// drawGrace
+}// End Grace
