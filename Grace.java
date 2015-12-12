47f6bc5c2b58c91e5fe67fa017e0bb86aa4c51bc
Malvin Herrera <malv.herrera@yahoo.com>
2015-12-12 14:57:06 -0500

    Update Grace.java

--- Grace.java
+++ Grace.java
@@ -4,21 +4,30 @@ public class Grace {
 	
 	private double x,y; //
 	
-	private int  width, height;
+	private int  width, height; // Grace's width and height
 	
 	private double gravity,speedV,speedH;
 	
-	private double weight;
+	private double weight; // Grace's weight that will make her come back down after jumping
+	
+	private boolean moveLeft, moveRight, jump = false; // Booleans for Grace's movements
 	
 	public void moveLeft(){
-		
+		moveLeft = true;
 	}
+	
 	public void moveRight(){
-		
+		moveRight = false;
 	}
+	
+	public void jump() {
+		jump = false;
+	}
+	
 	public void setHorizontalSpeed(double sH ){
 		
 	}
+	
 	public void drawGrace(){
 		
 	}
